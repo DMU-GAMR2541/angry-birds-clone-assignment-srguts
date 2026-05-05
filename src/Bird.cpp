6c4d43c886d91e40fa077f3051c1c2c7cd7f5c66
Nur Nur <P2717084@LEC-ADMIN.dmu.ac.uk>
2026-05-05 15:20:55 +0100

    added skeleton class for bird

---
+++ src/Bird.cpp
@@ -0,0 +1,13 @@
+#include "Bird.h"
+
+Bird::Bird(std::string texturePath, b2Vec2 position, b2World& world) {
+	// setup texture sprite and body here
+}
+
+void Bird::update() {
+	// update the sprite's position and rotation to match the physics body
+}
+
+void Bird::draw(sf::RenderWindow& window) {
+	// draw the sprite to the window
+}
