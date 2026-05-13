e0b5130a55ba63c94c062a839174a8e6506e59bf
srguts <nurabdirahim78@gmail.com>
2026-05-13 14:22:33 +0100

    blackbird update

--- src/BlackBird.cpp
+++ src/BlackBird.cpp
@@ -1,15 +1,14 @@
 #include "BlackBird.h"
 
 BlackBird::BlackBird(b2Vec2 position, b2World& world)
-	: Bird("../../../../assets/Ang_Birds/BlackBird.png", position, world, 0.5f, 0.5f, 0.5f) {   // higher density, lower restitution, larger scale
-    // any BlackBird specific setup here
+	: Bird("../../../../assets/Ang_Birds/BlackBird.png", position, world, 0.5f, 0.5f, 0.5f) {   // higher density, lower restitution, bigger scale
     // sp_rendered.setScale(0.2f, 0.2f);
 }
 
 void BlackBird::update() {
-    Bird::update();  // call the parent update
+    Bird::update();  // call update
 }
 
 void BlackBird::draw(sf::RenderWindow& window) {
-    Bird::draw(window);  // call the parent draw
+    Bird::draw(window);  // call draw
 }
