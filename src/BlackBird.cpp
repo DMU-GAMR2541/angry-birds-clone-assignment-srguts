c5af9913406fa5e6537f102032b0fb73b59a52b1
srguts <nurabdirahim78@gmail.com>
2026-05-14 03:27:28 +0100

    Update asset path for BlackBird image

--- src/BlackBird.cpp
+++ src/BlackBird.cpp
@@ -1,7 +1,7 @@
 #include "BlackBird.h"
 
 BlackBird::BlackBird(b2Vec2 position, b2World& world)
-	: Bird("../../../../assets/Ang_Birds/BlackBird.png", position, world, 0.5f, 0.5f, 0.5f) {   // higher density, lower restitution, bigger scale
+	: Bird("assets/Ang_Birds/BlackBird.png", position, world, 0.5f, 0.5f, 0.5f) {   // higher density, lower restitution, bigger scale
     // sp_rendered.setScale(0.2f, 0.2f);
 }
 
