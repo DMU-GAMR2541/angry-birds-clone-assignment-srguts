dda49a2128fa6fb7d39a19f2f3535d86c713372f
srguts <nurabdirahim78@gmail.com>
2026-05-14 03:30:17 +0100

    Fix asset path for RedBird image

--- src/RedBird.cpp
+++ src/RedBird.cpp
@@ -1,7 +1,7 @@
 #include "RedBird.h"
 
 RedBird::RedBird(b2Vec2 position, b2World& world)
-    : Bird("../../../../assets/Ang_Birds/RedBirdNoBG.png", position, world, 1.0f, 0.5f, 0.35f) {
+    : Bird("assets/Ang_Birds/RedBirdNoBG.png", position, world, 1.0f, 0.5f, 0.35f) {
     // sp_rendered.setScale(0.35f, 0.35f);
 }
 
